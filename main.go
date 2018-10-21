d2b752a61b5f3413a72bd7301866f8d42613d646
tamago <1129187334@qq.com>
2018-10-21 16:27:44 +0800

    增加jpg与png生成缩略图功能

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/nfnt/resize"
+)
+
+func resizeJPEG(srcPath string, dstDir string) error {
+	fp, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+	filename := filepath.Base(srcPath)
+	img, err := jpeg.Decode(fp)
+	//r := resize.Resize(128, 128, img, resize.Lanczos3)
+	r := resize.Thumbnail(128, 128, img, resize.Lanczos3)
+	dstPath := filepath.Join(dstDir, filename)
+	out, err := os.Create(dstPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(dstDir, 0666)
+			if err != nil {
+				return err
+			}
+			out, err = os.Create(dstPath)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+	defer out.Close()
+	jpeg.Encode(out, r, nil)
+	return nil
+}
+
+func resizePNG(srcPath string, dstDir string) error {
+	fp, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+	filename := filepath.Base(srcPath)
+	img, err := png.Decode(fp)
+	//r := resize.Resize(128, 128, img, resize.Lanczos3)
+	r := resize.Thumbnail(128, 128, img, resize.Lanczos3)
+	dstPath := filepath.Join(dstDir, filename)
+	out, err := os.Create(dstPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(dstDir, 0666)
+			if err != nil {
+				return err
+			}
+			out, err = os.Create(dstPath)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+	defer out.Close()
+	png.Encode(out, r)
+	return nil
+}
+
+// ResizePic 生成缩略图
+func ResizePic(srcPath string, dstDir string) error {
+	ext := path.Ext(srcPath)
+	switch strings.ToLower(ext) {
+	case ".jpg", ".jpeg":
+		return resizeJPEG(srcPath, dstDir)
+	case ".png":
+		return resizePNG(srcPath, dstDir)
+	}
+	return nil
+}
+
+var (
+	srcFile = flag.String("src", "test.jpg", "源文件")
+	dstDir  = flag.String("o", "dist", "输出文件目录")
+)
+
+func main() {
+	flag.Parse()
+	err := ResizePic(*srcFile, *dstDir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("resize success")
+}
